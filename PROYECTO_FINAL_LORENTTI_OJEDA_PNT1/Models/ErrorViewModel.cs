70f80d75a6129731d496a59df199e7cefe9d0f5d
Lautaro Gael Lorentti <lautaroglorentti@gmail.com>
2025-10-14 15:50:34 -0300

    Agregar archivos de proyecto.

---
+++ PROYECTO_FINAL_LORENTTI_OJEDA_PNT1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROYECTO_FINAL_LORENTTI_OJEDA_PNT1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
